6622e02cf2ddaa88d85b375aae40c2f34e272efb
Jordan <jordan.benson4@gmail.com>
2022-02-09 22:46:57 -0800

    Change to parameterized queries.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`${cohortName}`, limit];
+const stringQuery = `
 SELECT students.id as student_id, students.name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohortS.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohortS.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(stringQuery, values)
 .then(res => {
   res.rows.forEach((user) => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
